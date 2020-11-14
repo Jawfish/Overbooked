a3e4392fc15ed028c501bea80d8761d563c7e0c2
James Fitzgerald <james@jawfish.llc>
2020-11-14 11:17:22 -0500

    connect controls to animations

--- Actors/ActorBody.gd
+++ Actors/ActorBody.gd
@@ -6,6 +6,26 @@ export (Texture) var left_hand_texture: Texture = preload("res://Assets/Characte
 export (Texture) var right_hand_texture: Texture = preload("res://Assets/Characters/Monkey/HandR.png")
 export (Texture) var leg_texture: Texture = preload("res://Assets/Characters/Monkey/Leg.png")
 
+var actor: Actor
+
+onready var scale_x = scale.x
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact"):
+		$AnimationPlayer.play("Interact")
+
+func _enter_tree() -> void:
+	actor = get_parent()
+	actor.connect("moving", self, "play_walking_animation")
+	actor.connect("idle", self, "play_idle_animation")
+
+func _physics_process(delta: float) -> void:
+	# orient the sprites the correct direction
+	# if actor.direction.x is 0, don't change anything so the previous orientation is retained
+	if actor.direction.x < 0:
+		scale.x = -scale_x
+	elif actor.direction.x > 0:
+		scale.x = scale_x
 
 func _ready() -> void:
 	$BodyParts/Head/HeadSprite.texture = head_texture
@@ -14,3 +34,11 @@ func _ready() -> void:
 	$BodyParts/RightLeg/RightLegSprite.texture = leg_texture
 	$BodyParts/LeftHand/LeftHandSprite.texture = left_hand_texture
 	$BodyParts/RightHand/RightHandSprite.texture = right_hand_texture
+
+func play_walking_animation() -> void:
+	$AnimationPlayer.play("Walk")
+	$AnimationPlayer.playback_speed = 2
+
+func play_idle_animation() -> void:
+	$AnimationPlayer.play("Idle")
+	$AnimationPlayer.playback_speed = 1
--- Actors/Player.gd
+++ Actors/Player.gd
@@ -1,16 +1,29 @@
 extends Actor
 class_name Player
 
+enum STATES {IDLE, MOVING, INTERACTING}
+
+var current_state: int = STATES.IDLE
 
 func _physics_process(delta):
 	# get input action strength
-	var direction: Vector2
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 
 	# normalize it
 	direction = direction.normalized()
-
+	
+	# emit current player state as a signal so other things can know
+	# these are defined in the parent (Actor) class
+	if direction != Vector2.ZERO:
+		if not current_state == STATES.MOVING:		
+			current_state = STATES.MOVING
+			emit_signal("moving")
+	elif direction == Vector2.ZERO:
+		if not current_state == STATES.IDLE:
+			current_state = STATES.IDLE
+			emit_signal("idle")
+	
 	# move
 	# TODO: acceleration and deceleration
 	var movement = movement_speed * direction * delta
